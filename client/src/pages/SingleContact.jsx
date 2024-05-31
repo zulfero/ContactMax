ed26fef934dc554acd2ec1649cf68c9906dd5c65
zulfa Ramadhan <zulfar436@gmail.com>
2024-05-31 12:59:48 +0300

    finished working on the single contact section

--- client/src/pages/SingleContact.jsx
+++ client/src/pages/SingleContact.jsx
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 import phone from "../assets/images/phone.png";
 import email from "../assets/images/email-symbol-icon.svg";
 import image from "../assets/images/female.jpg";
-import person from "../assets/images/avatar-icon.svg"
+import person from "../assets/images/avatar-icon.svg";
+import birth from "../assets/images/calendar.png";
+import point from "../assets/images/location.png";
+import remove from "../assets/images/delete.png";
+import edit from "../assets/images/editing.png";
 
 function SingleContact() {
   return (
@@ -20,54 +24,67 @@ function SingleContact() {
 
           <h2 className="font-bold text-2xl">Natasha Romanof</h2>
         </div>
-        <div className="pl-[5em] flex flex-col text-white  text-center mt-[5em] gap-[5em]">
-          <div className="flex gap-[6em]">
+        <div className="pl-[5em] flex justify-between items-center my-[6em]  text-white  text-center mt-[5em] pr-[5em]">
+          <div className="flex flex-col gap-[6em]">
+            <div className="flex gap-[6em]">
+              <img src={person} alt="" width={30} />
+              <h1 className="text-xl text-white">Names:</h1>
+              <div className="flex gap-6">
+                <h2>Natasha </h2>
+                <h2>Romanof</h2>
+              </div>
+            </div>
+            <div className="flex gap-[6em]">
+              <div>
+                <Link to="/dashboard/mail">
+                  {" "}
+                  <img src={log} alt="" width={30} />{" "}
+                </Link>
+              </div>
+              <h1 className="text-xl text-white">Email:</h1>
+              <div className="flex ">
+                <h2>Marviniie@gmail.com</h2>
+              </div>
+            </div>{" "}
+            <div className="flex gap-[6em]">
+              <img className="text-black" src={phone} alt="" width={30} />
+              <h1 className="text-xl text-white">PhoneNo:</h1>
+              <div className="flex">
+                <h2>(00741)0743921270</h2>
+              </div>
+            </div>{" "}
+          </div>
 
-            <img src={person} alt="" width={30}/>
-            <h1 className="text-xl text-white">Names:</h1>
-            <div className="flex gap-6">
-              <h2>Natasha </h2>
-              <h2>Romanof</h2>
+          <div className="flex flex-col gap-[6em]">
+            <div className="flex gap-[6em]">
+              <img src={email} alt="" width={30} />
+              <h1 className="text-xl text-white">Address:</h1>
+              <div className="flex">
+                <h2>Magnolia horegion 1234</h2>
+              </div>
+            </div>{" "}
+            <div className="flex gap-[6em]">
+              <img src={point} alt="" width={30} />
+              <h1 className="text-xl text-white">Address:</h1>
+              <div className="flex">
+                <h2>Magnolia horegion 1234</h2>
+              </div>
+            </div>{" "}
+            <div className="flex gap-[6em]">
+              <img src={birth} alt="" width={30} />
+              <h1 className="text-xl text-white">BirthDate:</h1>
+              <div className="flex">
+                <h2>1994/02/3</h2>
+              </div>
             </div>
           </div>
-          <div className="flex gap-[6em]">
-            <div>
-              <Link to="/dashboard/mail">
-                {" "}
-                <img src={log} alt="" width={30} />{" "}
-              </Link>
-            </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <button>Edit Contact</button>
 
-            <h1 className="text-xl text-white">Email:</h1>
-            <div className="flex ">
-              <h2>Marviniie@gmail.com</h2>
-            </div>
-          </div>{" "}
-          <div className="flex gap-[6em]">
-            <img className="text-black" src={phone} alt="" width={30} />
-            <h1 className="text-xl text-white">PhoneNo:</h1>
-            <div className="flex">
-              <h2>(00741)0743921270</h2>
-            </div>
-          </div>{" "}
-          <div className="flex gap-[6em]">
+          <button>Send Email</button>
 
-            <img src={email} alt="" width={30}/>
-            <h1 className="text-xl text-white">Address:</h1>
-            <div className="flex">
-              <h2>Magnolia horegion 1234</h2>
-            </div>
-          </div>{" "}
-          <div className="flex gap-[6em]">
-            <h1 className="text-xl text-white">BirthDate:</h1>
-            <div className="flex">
-              <h2>1994/02/3</h2>
-            </div>
-          </div>
-          {/* <h1>Email</h1>
-          <h1>Phone Number</h1>
-          <h1>Address</h1>
-          <h1>Birth Date</h1> */}
+          <button>Delete Contact</button>
         </div>
       </Dashboard>
     </>
